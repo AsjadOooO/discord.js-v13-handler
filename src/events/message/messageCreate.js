d0f038a82d2cac8ae4148e2ac71460d833f135ba
Tejas Lamba <73745640+TajuModding@users.noreply.github.com>
2021-10-09 19:15:44 +0530

    Update messageCreate.js

--- src/events/message/messageCreate.js
+++ src/events/message/messageCreate.js
@@ -6,7 +6,7 @@ const prefix = client.config.prefix
 
   let isCommand = false;
   if (message.content.startsWith(prefix)) {
-    const args = message.content.replace(`${prefix}`, "").split(/\s+/);
+    const let args = message.content.slice(prefix.length).trim().split(/ +/g);
     const invoke = args.shift().toLowerCase();
     const cmd = client.getCommand(invoke);
 
