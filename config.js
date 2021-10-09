e93394188eb51c9837a20ad217fd7ced0ab7c631
Tejas Lamba <73745640+TajuModding@users.noreply.github.com>
2021-10-09 12:39:20 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  prefix: "!", //prefix
+  status: {
+         presence: '' // online , idle , dnd
+  },
+  token: "", // bot token
+  mongo: "" //mongo url if you want to connect to mongo, if not leave empty
+};
